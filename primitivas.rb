142104d5044762cf7c68285cb8f838ddaf8e5f1f
Ray Parra <rparragalaviz@gmail.com>
2018-08-24 08:52:52 -0700

    Add files via upload

---
+++ primitivas.rb
@@ -0,0 +1,30 @@
+#primitivas.rb
+
+puts "Hola mundo Ruby"
+
+puts 1+2
+
+a = 23
+b = 85
+c = 56
+d = true
+puts "La suma de a + b + c = ", a + b + c
+
+puts "El numero de horas en un dia es de 24hrs"
+
+puts "El numero de horas en una semana es de: ", 24*7
+puts "El numero de horas en un mes es de: ", 24*30
+puts "El numero de horas en un año es de: ", 24*366
+
+puts d
+
+
+if d then
+	puts "La sentencia es verdadera"
+else
+	puts "La sentencia es falsa"
+	end
+
+
+foo = gets
+puts "Escribiste esto: ", foo
