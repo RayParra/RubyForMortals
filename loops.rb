62a4c8eba26bd6a74243e8f2a2c0e35a0ba0407c
Ray Parra <rparragalaviz@gmail.com>
2018-08-28 12:10:45 -0700

    Add files via upload

---
+++ loops.rb
@@ -0,0 +1,36 @@
+# bucle loop
+i = 1
+
+loop do
+	puts i 
+	i += 1
+
+	break if i > 10
+end
+
+#bucle while
+j = 1
+while j < 10
+	puts j
+	j += 1
+end
+
+#bucle while condicion button
+k = 1
+begin
+	puts k
+	k += 1
+
+end while k < 10
+
+
+#bucle until
+l = 1
+until l < 10
+	puts l
+	l +=1 
+end
+
+for x in (1..10)
+	puts x
+end
