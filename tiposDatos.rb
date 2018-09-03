c992e20a131d7740e9c7b08d4cfaa26179343b79
Databack <rparragalaviz@gmail.com>
2018-09-03 06:32:45 -0700

    tipos de datos en ruby

---
+++ tiposDatos.rb
@@ -0,0 +1,32 @@
+#tiposDatos.rb
+
+# Integers
+
+# foo = 10 
+
+# bar = 20
+
+# puts foo + bar
+
+# puts "foo =  #{foo}"
+# puts "bar = #{bar}"
+
+
+# Float
+
+foo = 24.54
+
+bar = 89.23
+
+puts foo + bar
+
+puts "foo = #{foo}"
+puts "bar = #{bar}"
+
+# Boolean
+
+t = true
+f = false
+
+puts "t #{t}"
+puts "f #{f}"
